49a76a4996db20fd7a6b3851a87cbb06bdf3c708
soheil alimadadi <soheilblizzard@gmail.com>
2023-07-03 05:29:36 +0330

    refa(database): refactor database code

--- database/base.py
+++ database/base.py
@@ -2,4 +2,13 @@ from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
+    """
+    A base class for declarative models in SQLAlchemy.
+
+    This class is used as a base class for declarative models in SQLAlchemy.
+    It inherits from the `DeclarativeBase` class provided by SQLAlchemy.
+    Using this class as a base allows for easy customization of the declarative
+    model base class.
+    """
     pass
+
--- database/core.py
+++ database/core.py
@@ -3,18 +3,67 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from helpers.designs import Singleton
-from kernel.settings import DB_URL
+from kernel.settings.database import DB_URL
 
 
 class SqlAlchemy(metaclass=Singleton):
+    """
+    A singleton class for creating a SQLAlchemy engine and session.
+
+    This class provides a single instance of a SQLAlchemy engine and session
+    that can be used throughout the application. It uses the Singleton pattern
+    to ensure that only one instance of the class is created.
+
+    Attributes
+    ----------
+    engine : Engine
+        The SQLAlchemy engine instance.
+    session : Session
+        The SQLAlchemy session instance.
+
+    Methods
+    -------
+    create_engine() -> Engine:
+        Creates a new SQLAlchemy engine instance.
+    create_session() -> Session:
+        Creates a new SQLAlchemy session instance.
+
+    Examples
+    --------
+    >>> db = SqlAlchemy()
+    >>> engine = db.engine
+    >>> session = db.session
+    """
+
     def __init__(self) -> None:
+        """
+        Initializes the SqlAlchemy instance.
+
+        Creates a new SQLAlchemy engine and session instance.
+        """
         self.engine = self.create_engine()
         self.session = self.create_session()
 
     def create_engine(self) -> Engine:
+        """
+        Creates a new SQLAlchemy engine instance.
+
+        Returns
+        -------
+        Engine
+            A new SQLAlchemy engine instance.
+        """
         engine = create_engine(DB_URL)
         return engine
 
     def create_session(self) -> Session:
+        """
+        Creates a new SQLAlchemy session instance.
+
+        Returns
+        -------
+        Session
+            A new SQLAlchemy session instance.
+        """
         session = Session(self.engine)
         return session
